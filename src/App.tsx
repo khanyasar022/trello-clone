ec72d0ebcd3d4a8ad1d9859121e26a6cc9df23d4
khanyasar022 <38328077+khanyasar022@users.noreply.github.com>
2022-03-18 17:14:26 +0530

    card component v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import design from './assets/img-design.png'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen grid grid-rows-[40px_50px_1fr] leading-5 bg-sky-600 ">
+      <div className="flex items-center " >
+        <nav className="pl-4 text-[1.5rem] w-full leading-10 bg-sky-700">App bar</nav>
+      </div>
+      <div className="pl-4 flex items-center" >
+        <nav className="text-[1.1rem] bg-sky-600">Board bar</nav>
+      </div>
+      <div className="flex overflow-x-auto m=[0_10px_10px] " >
+        <div className="flex-[0_0_auto] w-[300px] h-[calc(100%-10px)] p-[0_8px] bg-slate-200 rounded-t-[5px] ml-[10px] " >
+          <header className="pl-[15px] h-9 leading-9 text-[16px] font-bold " >Brainstorm</header>
+          <ul className="list-none m-0 overflow-y-auto max-h-[calc(100%-36px-36px)] " >
+            <li className="bg-white pb-2.5 mb-2.5 rounded-t-[5px] shadow-[0_1px_1px_rgba(0,0,0,0.1)] ">
+              <img src={design} alt="" className="m-[0] rounded-t-[3px] block w-[calc(100% + 20px)] " />
+              <p className="pl-2.5">Design & Research</p>
+            </li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+            <li className="bg-white p-2.5 mb-2.5 ">second</li>
+          </ul>
+          <div className="rounded-[3px]" >
+          <footer className="pl-[15px] h-9 leading-9 " >Add another card</footer>
+          </div>
+        </div>
+
+        
+      </div>
     </div>
   );
 }
